b990456ac1c68bfc6d64834bd09cd4e2f271d81d
Priscila <priscila.rodrigues@cjr.org.br>
2020-10-31 11:26:01 -0300

    feat: implementa 2 novos componentes

---
+++ src/components/Pagination/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+const Pagination = () => {
+    return(
+        <div className='Pagination'>
+            <span> 1 </span>
+            <p> 2 </p>
+            <p> ... </p>
+            <p> 9 </p>
+        </div>
+    )
+}
+export default Pagination;
