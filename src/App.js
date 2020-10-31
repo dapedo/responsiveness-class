f7e6d3906e68e865720ec07be164db0aec38ccca
Priscila <priscila.rodrigues@cjr.org.br>
2020-10-31 11:36:55 -0300

    feat: adiciona na pagina principal as componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Pagination from './components/Pagination';
+import Profile from './components/Profile';
+import Search from './components/Search';
 function App() {
-  return <h1>Hello World!</h1>
+  return(
+    <div>
+      <Header/>
+      <Profile/>
+      <Navigation/>
+      <Search/>
+      <Pagination/>
+    </div>
+  )
 }
 
 export default App
